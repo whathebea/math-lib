75c0090968f02d77196753e03c75280288108871
whathebea <bea.graciast@gmail.com>
2022-11-12 16:02:50 -0300

    commiting maven project

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,33 @@
+package calculator;
+
+public class Calculator {
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+
+    public static double modulo(double a, double b) {
+        return a % b;
+    }
+
+    public static double power(double a, double b) {
+        return Math.pow(a, b);
+    }
+
+    public static double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+
+}
